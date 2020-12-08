6fee322e686f7f05d1a5747089986790f1f82ea9
Toan39 <71543094+Toan39@users.noreply.github.com>
2020-12-08 14:58:22 +0100

    dapper20

---
+++ TijdFunction1/TijdFunction1/DataService.cs
@@ -0,0 +1,45 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using Dapper;
+using System.Collections;
+
+namespace TimeFunction1
+{
+    public static class DataService
+    {
+        public static List<quetime> QTimes()
+        {
+            string connectionString;
+            string sql;
+
+            connectionString = @"Data Source=localhost; Initial Catalog=Tim123; Integrated Security=True ";
+            sql = "select * from TheDataQueTime";
+
+            using (var connect = new SqlConnection(connectionString))
+            {
+                var result = connect.Query<quetime>(sql).ToList();
+                return result;
+            }
+        }
+
+        public static List<walktime> WTimes()
+        {
+            string connectionString;
+            string sql;
+
+            connectionString = @"Data Source=localhost; Initial Catalog=Tim123; Integrated Security=True ";
+            sql = "select * from TheDataWalkTime";
+
+            using (var connect = new SqlConnection(connectionString))
+            {
+                var result = connect.Query<walktime>(sql).ToList();
+                return result;
+            }
+        }
+    }
+}
---
+++ TijdFunction1/TijdFunction1/quetime.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using Dapper;
+using System.Collections;
+
+namespace TimeFunction1
+{
+        public class quetime
+        {
+            public string Number { get; set; }
+            public string Name { get; set; }
+            public float AverageQueTime { get; set; }
+            public float RideTime { get; set; }
+        }
+}
