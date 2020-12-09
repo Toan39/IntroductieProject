77da8bd7bba6823a08b1896f5edc0c9d7d2cb1bd
SusanRings <71766861+SusanRings@users.noreply.github.com>
2020-12-09 16:45:30 +0100

    .

--- WindowsFormsApp5/WindowsFormsApp5/Form1.cs
+++ WindowsFormsApp5/WindowsFormsApp5/Form1.cs
@@ -22,12 +22,6 @@ namespace WindowsFormsApp5
 
         private void gMapControl1_Load(object sender, EventArgs e)
         {
-            /*
-            gmap.MapProvider = GMap.NET.MapProviders.BingMapProvider.Instance;
-            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
-            gmap.Position = new GMap.NET.PointLatLng(48.8589507, 2.2775175);
-            gmap.ShowCenter = false;
-            */
             
             gmap.MapProvider = GMapProviders.GoogleMap;
             gmap.Position = new GMap.NET.PointLatLng(48.872618, 2.776765/*48.867374, 2.784018*/); 
@@ -36,8 +30,17 @@ namespace WindowsFormsApp5
             gmap.Zoom = 16;
             gmap.DragButton = MouseButtons.Left;
 
+            PointLatLng start = new PointLatLng(51.84906928092645, 4.674627074271992/*48.87273928374085, 2.7780222603896094*/);
+            PointLatLng end = new PointLatLng(51.83607539301313, 4.690368966202675/*48.873275591357896, 2.776069612232649*/);
+            MapRoute route = GMap.NET.MapProviders.GoogleMapProvider.Instance.GetRoute(start, end, true, true, 15);
+
+            GMapRoute r = new GMapRoute(route.Points, "My route");
+            GMapOverlay routesOverlay = new GMapOverlay("routes");
+            routesOverlay.Routes.Add(r);
+            gmap.Overlays.Add(routesOverlay);
+
         }
-        
+
         // disable the red cross
         private void Form1_Load(object sender, EventArgs e)
         {
