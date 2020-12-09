a9e003d19d0dd27ab26e406fa221c522f67af52c
36mb <74769682+36mb@users.noreply.github.com>
2020-12-09 14:37:38 +0100

    tijdfunctie localhost 2

--- TijdFunction1/TijdFunction1/DataService.cs
+++ TijdFunction1/TijdFunction1/DataService.cs
@@ -17,7 +17,7 @@ namespace TimeFunction1
             string connectionString;
             string sql;
 
-            connectionString = @"Data Source=localhost;Initial Catalog=master;Integrated Security=True";
+            connectionString = @"Data Source=localhost;Initial Catalog=Tim123;Integrated Security=True";
             sql = "select * from TheDataQueTime";
 
             using (var connect = new SqlConnection(connectionString))
@@ -32,7 +32,7 @@ namespace TimeFunction1
             string connectionString;
             string sql;
 
-            connectionString = @"Data Source=localhost;Initial Catalog=master;Integrated Security=True";
+            connectionString = @"Data Source=localhost;Initial Catalog=Tim123;Integrated Security=True";
             sql = "select * from TheDataWalkTime";
 
             using (var connect = new SqlConnection(connectionString))
