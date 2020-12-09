8794c18008bd449773a69a1f1091e1e64bd75904
SusanRings <71766861+SusanRings@users.noreply.github.com>
2020-12-09 15:16:32 +0100

    disneyland op de map

--- WindowsFormsApp5/WindowsFormsApp5/Form1.Designer.cs
+++ WindowsFormsApp5/WindowsFormsApp5/Form1.Designer.cs
@@ -66,6 +66,7 @@
             this.Controls.Add(this.gmap);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
--- WindowsFormsApp5/WindowsFormsApp5/Form1.cs
+++ WindowsFormsApp5/WindowsFormsApp5/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GMap.NET.WindowsForms;
 
 namespace WindowsFormsApp5
 {
@@ -21,11 +22,26 @@ namespace WindowsFormsApp5
 
         private void gMapControl1_Load(object sender, EventArgs e)
         {
+            /*
             gmap.MapProvider = GMap.NET.MapProviders.BingMapProvider.Instance;
             GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
             gmap.Position = new GMap.NET.PointLatLng(48.8589507, 2.2775175);
             gmap.ShowCenter = false;
+            */
+            
+            gmap.MapProvider = GMapProviders.GoogleMap;
+            gmap.Position = new GMap.NET.PointLatLng(48.872618, 2.776765/*48.867374, 2.784018*/); 
+            gmap.MinZoom = 5;
+            gmap.MaxZoom = 100;
+            gmap.Zoom = 16;
+            gmap.DragButton = MouseButtons.Left;
 
+        }
+        
+        // disable the red cross
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gmap.ShowCenter = false; 
         }
     }
 }
