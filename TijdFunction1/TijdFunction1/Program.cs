c650785aeb7d6b050ae1f736f4c680ba37ee31ec
Toan39 <71543094+Toan39@users.noreply.github.com>
2020-12-10 11:50:35 +0100

    dapper22

--- TijdFunction1/TijdFunction1/Program.cs
+++ TijdFunction1/TijdFunction1/Program.cs
@@ -19,8 +19,9 @@ namespace TimeFunction1
         DataGridView dat_1;
         DataGridView dat_2;
         List<float> totaltime = new List<float>();
+        List<float> finaltime = new List<float>();
         public TimeMethod()
-        {  
+        {
             CultureInfo.CurrentCulture = new CultureInfo("en-US", false);
             ////declarations of panel////
 
@@ -31,9 +32,9 @@ namespace TimeFunction1
             route.Location = new Point(600, 100);
             this.Controls.Add(route);
             route.Click += new EventHandler(Button1);
-      
+
             // Viewer 1//
-            dat_1= new DataGridView();
+            dat_1 = new DataGridView();
             dat_1.Size = new Size(500, 500);
             dat_1.Location = new Point(200, 120);
             this.Controls.Add(dat_1);
@@ -48,39 +49,63 @@ namespace TimeFunction1
         ////void methods////
         private void Button1(object o, EventArgs ea)
         {
-            
+
             dat_1.DataSource = DataService.QTimes();
             dat_2.DataSource = DataService.WTimes();
             int i = 0;
+
             foreach (quetime Number in DataService.QTimes())
             {
-               if (DataService.QTimes().Count < 45)     //if statement onnodig
+                if (DataService.QTimes().Count < 45)     //if statement onnodig
                 {
                     totaltime.Add(new float());
-                    totaltime[i] = DataService.QTimes()[i].AverageQueTime + DataService.QTimes()[i].RideTime; ;
-                    Console.WriteLine(totaltime[i]);
+                    totaltime[i] = DataService.QTimes()[i].AverageQueTime + DataService.QTimes()[i].RideTime;
+                    //Console.WriteLine(totaltime[i]);
                     i++;
-                    
+
                 }
-
             }
-            //var totaltime = DataService.AvgQTimes().Zip(DataService.RTime(), (first, second) => first + second);
 
-            //foreach (var item in totaltime)
-            //{
-            //    Console.WriteLine(item);
-            //}
-            //var totaltime = zip(DataService.AvgQTimes , DataService.RTime)];
+            for (int p = 0; p < 1893; p++)
+            {
+                for (int j = 0; j < 44; j++)
+                {
+                    for (int k = 0; k < 45; k++)
+                    {
+
+                        finaltime.Add(new float());
+                        totaltime.Add(new float());
+                        finaltime[j] = totaltime[k] + DataService.WTimes()[p].WalkTime;
+                        //Console.WriteLine(finaltime[j]);
+                    }
+                }
+            }
+
+
+    
+
+
+
         }
-       
 
-        public void Time(int waitTime, int walkdistance)
-        {            
-            //double walkTime;
-            //double estimatedTime = queTime + ridetime+ walkTime;
-        }
 
+        //var totaltime = DataService.AvgQTimes().Zip(DataService.RTime(), (first, second) => first + second);
+
+        //foreach (var item in totaltime)
+        //{
+        //    Console.WriteLine(item);
+        //}
+        //var totaltime = zip(DataService.AvgQTimes , DataService.RTime)];
     }
+
+
+    //public void Time(int waitTime, int walkdistance)
+    //{            
+    //double walkTime;
+    //double estimatedTime = queTime + ridetime+ walkTime;
+    // }
+
+
 
     class Timefunction1
     {
@@ -92,3 +117,4 @@ namespace TimeFunction1
         }
     }
 }
+
--- TijdFunction1/TijdFunction1/walktime.cs
+++ TijdFunction1/TijdFunction1/walktime.cs
@@ -12,7 +12,7 @@ namespace TimeFunction1
     public class walktime
     {
         public string StartPoint { get; set; }
-        public string Endpoint { get; set; }
+        public string EndPoint { get; set; }
         public float Distance { get; set; }
         public float WalkTime { get; set; }
     }
