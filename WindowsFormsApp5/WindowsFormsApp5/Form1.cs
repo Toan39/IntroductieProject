60d6c0f0b0a025415c1acfc25b6001eda90b17d0
vinsko <vincent.pattinama@gmail.com>
2020-12-10 17:00:58 +0100

    .

--- WindowsFormsApp5/WindowsFormsApp5/Form1.cs
+++ WindowsFormsApp5/WindowsFormsApp5/Form1.cs
@@ -1,6 +1,7 @@
-﻿using GMap.NET.MapProviders;
-using GMap.NET;
-using GMap;
+﻿using GMap.NET;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.MapProviders;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -10,12 +11,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using GMap.NET.WindowsForms;
+using System.Resources;
+using System.Reflection;
+
+
 
 namespace WindowsFormsApp5
 {
     public partial class Form1 : Form
     {
+        List<Point> locations;
+        List<string> attractions;
         public Form1()
         {
             InitializeComponent();
@@ -26,22 +32,46 @@ namespace WindowsFormsApp5
 
             gmap.MapProvider = GMapProviders.GoogleMap;
             GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
-            gmap.Position = new GMap.NET.PointLatLng(-25.974134, 32.593042/*48.867374, 2.784018*/);
+            gmap.Position = new GMap.NET.PointLatLng(48.872562, 2.773616/*48.867374, 2.784018*/);
             gmap.MinZoom = 5;
             gmap.MaxZoom = 100;
             gmap.Zoom = 16;
             gmap.DragButton = MouseButtons.Left;
+            GMapOverlay markers = new GMapOverlay("markers");
+            ResourceManager resourcemanager= new ResourceManager("WindowsFormsApp5.Properties.Resources", Assembly.GetExecutingAssembly());
 
-            PointLatLng start = new PointLatLng(-25.974134, 32.593042);
-            PointLatLng end = new PointLatLng(-25.959048, 32.592827);
-            
-            MapRoute route = GoogleMapProvider.Instance.GetRoute(start, end, false, false, 15);
+            PointLatLng adventure = new PointLatLng(48.872562, 2.773616);
+            PointLatLng pirates = new PointLatLng(48.873491, 2.773383);
+            PointLatLng world = new PointLatLng(48.874590, 2.776050);
+            GMapMarker[] mark;
+            mark = new GMapMarker[1000];
+           
+                //string mark = "marker" + t;
+                bool c;
+                
+                for(int t = 0; t<attractions.Count; t++)
+                {
+                GMapMarker result = new GMarkerGoogle(attraction.Location, GMarkerGoogleType.blue_pushpin);
+                markers.Markers.Add(result);
+                gmap.Overlays.Add(markers);
+                result.ToolTipText = t.ToString();
+                mark[t] = result;
+                }
 
-            GMapRoute r = new GMapRoute(route.Points, "My route");
-            r.Stroke = new Pen(Color.Red, 20);
-            GMapOverlay routesOverlay = new GMapOverlay("routes");
-            routesOverlay.Routes.Add(r);
-            gmap.Overlays.Add(routesOverlay);
+            //GMapMarker marker1 = new GMarkerGoogle(adventure, GMarkerGoogleType.blue_pushpin);
+            /*
+            GMapMarker marker2 = new GMarkerGoogle(pirates, GMarkerGoogleType.blue_pushpin);
+            markers.Markers.Add(marker2);
+            gmap.Overlays.Add(markers);
+            GMapMarker marker3 = new GMarkerGoogle(world, GMarkerGoogleType.blue_pushpin);
+            markers.Markers.Add(marker3);
+            gmap.Overlays.Add(markers);
+            marker1.ToolTipMode = MarkerTooltipMode.Always;
+            marker2.ToolTipMode = MarkerTooltipMode.Always;
+            marker3.ToolTipMode = MarkerTooltipMode.Always;
+            marker1.ToolTipText = "1";
+            marker2.ToolTipText = "2";
+            marker3.ToolTipText = "3";*/
         }
 
             // disable the red cross
