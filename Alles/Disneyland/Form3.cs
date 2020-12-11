7b9035358694cf7cec181392c732c4a4be5d4bf0
Toan39 <71543094+Toan39@users.noreply.github.com>
2020-12-11 21:01:00 +0100

    hgdd

--- Alles/Disneyland/Form3.cs
+++ Alles/Disneyland/Form3.cs
@@ -16,9 +16,6 @@ namespace Disneyland
 
         //varabiales declarations//
         
-            List<Rij> datalist = new List<Rij>();
-            List<string> usedpoints = new List<string>();
-        
         
         public Form3()
          {
@@ -29,156 +26,15 @@ namespace Disneyland
         ////void methods////
             
 
-            void sorteer()
-            {
-                datalist = datalist.OrderBy(x => x.TotalTime).ToList();
-            }
-
-            public string returnlowest(int Inserted)
-            {
-                string lowest = "";
-                float verbruiktetijd = 0;
-                string previous = "";
-                for (int i = 0; (verbruiktetijd + datalist[i].TotalTime) < Inserted; i++)
-                {
-
-                    if (possible(datalist[i].EndPoint.ToString()) && begincheck(datalist[i].StartPoint.ToString(), previous))
-                    {
-                        lowest = lowest + datalist[i].EndPoint.ToString() + "\n";
-                        usedpoints.Add(datalist[i].EndPoint.ToString());
-                        verbruiktetijd = verbruiktetijd + datalist[i].TotalTime;
-                        previous = datalist[i].EndPoint.ToString();
-                        i = 0;
-                    }
-                }
-                return (lowest);
-            }
-
-            public void makelist()
-            {
-
-                for (int p = 0; p < (usedpoints.Count); p++)
-                {
-                    int k = 0;
-                    foreach (quetime Name in DataService.QTimes())
-                    {
-                        if (DataService.QTimes()[k].Number == usedpoints[p])
-                        {
-
-                            DataService.att.Add(new attraction());
-                            DataService.att[p].Name = DataService.QTimes()[k].Name;
-                            DataService.att[p].Lat = DataService.QTimes()[k].Lat;
-                            DataService.att[p].Lon = DataService.QTimes()[k].Lon;
-                        }
-                        k++;
-                    }
-                   
-                }
-            }
-
-            public bool possible(string x)
-            {
-                foreach (string endpoint in usedpoints)
-                {
-                    if (x == endpoint)
-                    {
-                        return false;
-                    }
-                }
-                return true;
-            }
-
-            public bool begincheck(string y, string z)
-            {
-                if (z != "")
-                {
-                    if (y == z)
-                    {
-                        return true;
-                    }
-                    else
-                    {
-                        return false;
-                    }
-                }
-                else
-                {
-                    return true;
-                }
-            }
-
-        internal class Rij
-        {
-            public string StartPoint;
-            public string EndPoint;
-            public float Distance;
-            public float WalkTime;
-            public float TotalTime;
-        }
-        public class quetime
-        {
-            public string Number { get; set; }
-            public string Name { get; set; }
-            public float AverageQueTime { get; set; }
-            public float RideTime { get; set; }
-
-            public double Lat { get; set; }
-            public double Lon { get; set; }
-        }
-
-        public class walktime
-        {
-            public string StartPoint { get; set; }
-            public string EndPoint { get; set; }
-            public float Distance { get; set; }
-            public float WalkTime { get; set; }
-            public float TotalTime { get; set; }
-        }
-
-        public static class DataService
-        {
-            public static List<quetime> QTimes()
-            {
-                string connectionString;
-                string sql;
-
-                connectionString = @"Data Source=localhost;Initial Catalog=Tim123;Integrated Security=True";
-                sql = "select * from TheDataQueTime";
-
-                using (var connect = new SqlConnection(connectionString))
-                {
-                    var result = connect.Query<quetime>(sql).ToList();
-                    return result;
-                }
-            }
-
-            public static List<walktime> WTimes()
-            {
-                string connectionString;
-                string sql;
-
-                connectionString = @"Data Source=localhost;Initial Catalog=Tim123;Integrated Security=True";
-                sql = "select * from TheDataWalkTime";
-
-                using (var connect = new SqlConnection(connectionString))
-                {
-                    var result = connect.Query<walktime>(sql).ToList();
-                    return result;
-                }
-
-            
-            }
-            public static List<attraction> att = new List<attraction>();
-        }
        
 
         public void button1_Click(object sender, EventArgs e)
         {
-            Form5 map = new Form5();
+            Form5 map = new Form5(comboBox1.Text);
             //map.attractionlist = DataService.att;
             String s = comboBox1.Text;
             string a = comboBox2.Text;
-            int InsertedTime = 100;  /*int.Parse(comboBox1.Text.ToString()) * 60;*/
+            
             int x = 0;
             int q = 0;
 
@@ -205,20 +61,7 @@ namespace Disneyland
               this.Hide();
             }
 
-            int i = 0;
-            foreach (walktime startpoint in DataService.WTimes())
-            {
-                datalist.Add(new Rij());
-                datalist[i].StartPoint = DataService.WTimes()[i].StartPoint;
-                datalist[i].EndPoint = DataService.WTimes()[i].EndPoint;
-                datalist[i].Distance = DataService.WTimes()[i].Distance;
-                datalist[i].WalkTime = DataService.WTimes()[i].WalkTime;
-                datalist[i].TotalTime = DataService.WTimes()[i].TotalTime;
-                i++;
-            }
-            sorteer();
-            returnlowest(InsertedTime);
-            makelist();
+           
         }
 
         private void checkedListBox_SelectedIndexChanged(object sender, EventArgs e)
--- Alles/Disneyland/Form5.cs
+++ Alles/Disneyland/Form5.cs
@@ -20,21 +20,44 @@ namespace Disneyland
 {
     public partial class Form5 : Form
     {
-        public Form5()
+        List<Rij> datalist = new List<Rij>();
+        List<string> usedpoints = new List<string>();
+        public Form5(string tijd)
         {
             InitializeComponent();
+            int InsertedTime =30 /*int.Parse(tijd)*/;
+            int i = 0;
+            foreach (walktime startpoint in DataService.WTimes())
+            {
+                datalist.Add(new Rij());
+                datalist[i].StartPoint = DataService.WTimes()[i].StartPoint;
+                datalist[i].EndPoint = DataService.WTimes()[i].EndPoint;
+                datalist[i].Distance = DataService.WTimes()[i].Distance;
+                datalist[i].WalkTime = DataService.WTimes()[i].WalkTime;
+                datalist[i].TotalTime = DataService.WTimes()[i].TotalTime;
+                i++;
+            }
+            sorteer();
+            returnlowest(InsertedTime);
+            makelist();
 
-            for (int t = 0; t < Form3.DataService.att.Count; t++)
+
+            for (int t = 0; t < Lijst.att.Count; t++)
             {
                 int x = t + 1;
                 string result = "";
-                result = x.ToString() + ". " + result + Form3.DataService.att[t].Name + "\n";
+                result = x.ToString() + ". " + result + Lijst.att[t].Name + "\n";
                 label2.Text = label2.Text + result;
             }
 
             // System.Diagnostics.Process.Start("https://www.disneylandparis.com/nl-nl/plattegronden/");
         }
-   
+
+
+        public static class Lijst
+        {
+            public static List<attraction> att = new List<attraction>();
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -47,6 +70,96 @@ namespace Disneyland
             Form3 rides = new Form3();
             rides.Show();
             this.Hide();
+        }
+
+        void sorteer()
+        {
+            datalist = datalist.OrderBy(x => x.TotalTime).ToList();
+        }
+
+        public string returnlowest(int Inserted)
+        {
+            string lowest = "";
+            float verbruiktetijd = 0;
+            string previous = "";
+            for (int i = 0; (verbruiktetijd + datalist[i].TotalTime) < Inserted; i++)
+            {
+
+                if (possible(datalist[i].EndPoint.ToString()) && begincheck(datalist[i].StartPoint.ToString(), previous))
+                {
+                    lowest = lowest + datalist[i].EndPoint.ToString() + "\n";
+                    usedpoints.Add(datalist[i].EndPoint.ToString());
+                    verbruiktetijd = verbruiktetijd + datalist[i].TotalTime;
+                    previous = datalist[i].EndPoint.ToString();
+                    i = 0;
+                }
+            }
+            return (lowest);
+        }
+
+
+
+        public bool possible(string x)
+        {
+            foreach (string endpoint in usedpoints)
+            {
+                if (x == endpoint)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool begincheck(string y, string z)
+        {
+            if (z != "")
+            {
+                if (y == z)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        internal class Rij
+        {
+            public string StartPoint;
+            public string EndPoint;
+            public float Distance;
+            public float WalkTime;
+            public float TotalTime;
+        }
+        
+
+        public void makelist()
+        {
+
+            for (int p = 0; p < (usedpoints.Count); p++)
+            {
+                int k = 0;
+                foreach (quetime Name in DataService.QTimes())
+                {
+                    if (DataService.QTimes()[k].Number == usedpoints[p])
+                    {
+
+                        Lijst.att.Add(new attraction());
+                        Lijst.att[p].Name = DataService.QTimes()[k].Name;
+                        Lijst.att[p].Lat = DataService.QTimes()[k].Lat;
+                        Lijst.att[p].Lon = DataService.QTimes()[k].Lon;
+                    }
+                    k++;
+                }
+
+            }
         }
 
         private void gmap_Load(object sender, EventArgs e)
@@ -65,9 +178,9 @@ namespace Disneyland
 
 
 
-            for (int t = 0; t < Form3.DataService.att.Count; t++)
+            for (int t = 0; t < Lijst.att.Count; t++)
             {
-                PointLatLng p = new PointLatLng(Form3.DataService.att[t].Lat, Form3.DataService.att[t].Lon);
+                PointLatLng p = new PointLatLng(Lijst.att[t].Lat, Lijst.att[t].Lon);
                 GMapMarker marker = new GMarkerGoogle(p, GMarkerGoogleType.blue_pushpin);
                 markers.Markers.Add(marker);
                 gmap.Overlays.Add(markers);
@@ -77,6 +190,63 @@ namespace Disneyland
             }
         }
     }
+
+    public static class DataService
+    {
+        public static List<quetime> QTimes()
+        {
+            string connectionString;
+            string sql;
+
+            connectionString = @"Data Source=localhost;Initial Catalog=Tim123;Integrated Security=True";
+            sql = "select * from TheDataQueTime";
+
+            using (var connect = new SqlConnection(connectionString))
+            {
+                var result = connect.Query<quetime>(sql).ToList();
+                return result;
+            }
+        }
+
+        public static List<walktime> WTimes()
+        {
+            string connectionString;
+            string sql;
+
+            connectionString = @"Data Source=localhost;Initial Catalog=Tim123;Integrated Security=True";
+            sql = "select * from TheDataWalkTime";
+
+            using (var connect = new SqlConnection(connectionString))
+            {
+                var result = connect.Query<walktime>(sql).ToList();
+                return result;
+            }
+
+
+        }
+    }
+
+
+    public class quetime
+    {
+        public string Number { get; set; }
+        public string Name { get; set; }
+        public float AverageQueTime { get; set; }
+        public float RideTime { get; set; }
+
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+    }
+
+    public class walktime
+    {
+        public string StartPoint { get; set; }
+        public string EndPoint { get; set; }
+        public float Distance { get; set; }
+        public float WalkTime { get; set; }
+        public float TotalTime { get; set; }
+    }
+
     public class attraction
     {
         public string Name;
