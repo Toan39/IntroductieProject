c59a13ddb7d515959652aa2b94da5355ccaad71d
Toan39 <71543094+Toan39@users.noreply.github.com>
2021-01-14 15:56:12 +0100

    comboxgone

--- Alles/Disneyland/Form3.cs
+++ Alles/Disneyland/Form3.cs
@@ -21,12 +21,12 @@ namespace Disneyland
          {
             CultureInfo.CurrentCulture = new CultureInfo("en-US", false);
             InitializeComponent();
-            comboBox1.SelectedIndex = 1; //REMOVE DIT LATER
-            comboBox2.SelectedIndex = 1; //REMOVE DIT LATER
+            /* comboBox1.SelectedIndex = 1;*/ //add combobox
+            /*comboBox2.SelectedIndex = 1;*/ //add combobox
         }
 
         ////void methods////
-        
+
 
         public void button1_Click(object sender, EventArgs e)
         {
@@ -34,33 +34,37 @@ namespace Disneyland
 
             Form5 map = new Form5(selecteditems);
             //map.attractionlist = DataService.att;
-            String s = comboBox1.Text;
-            string a = comboBox2.Text;
-            
+
+            //////add combobox
+            //String s = comboBox1.Text; 
+            //string a = comboBox2.Text;
+
             int x = 0;
             int q = 0;
 
-            try
-            {x = int.Parse(s);}
-            catch(Exception)
-            {}
-            try
-            {q = int.Parse(a);}
-            catch(Exception)
-            {}
+            //////add combobox
+            //try
+            //{x = int.Parse(s);}
+            //catch(Exception)
+            //{}
+            //try
+            //{q = int.Parse(a);}
+            //catch(Exception)
+            //{}
 
             if (x + q > 12)
             {MessageBox.Show("Dit is meer dan 12 uur, dan is disneyland al dicht. Selecteer andere tijden.");}
-            if(comboBox1.SelectedItem == null)
-            {MessageBox.Show("Er is geen tijd doorgegeven.");}
-            if (comboBox2.SelectedItem == null)
-            { MessageBox.Show("Er is geen tijd doorgegeven."); }
+            ////////add combobox
+            //if(comboBox1.SelectedItem == null)
+            //{MessageBox.Show("Er is geen tijd doorgegeven.");}
+            //if (comboBox2.SelectedItem == null)
+            //{ MessageBox.Show("Er is geen tijd doorgegeven."); }
             if (checkedListBox.SelectedItem == null)
             { MessageBox.Show("Er zijn geen attracties doorgegeven"); }
             if (listBoxItem.Items.Count >18)
             { MessageBox.Show("Mag niet meer dan 18 attracties selecteren"); }
 
-            if (x+q<10 && checkedListBox.SelectedItem!=null && comboBox1.SelectedItem!=null && comboBox2.SelectedItem!=null && listBoxItem.Items.Count < 18)
+            if (x+q<10 && checkedListBox.SelectedItem!=null && /*comboBox1.SelectedItem!=null && comboBox2.SelectedItem!=null &&*/ listBoxItem.Items.Count < 18)
             { 
               map.Show();
               this.Hide();
